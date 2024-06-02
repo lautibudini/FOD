c8025d36c2f64ee9c45b72ec8e20cbf75246fef6
Lautaro Tomas Budini <lautarobudini5@gmail.com>
2024-06-02 14:56:19 -0300

    ejercicio traqui por suerte sjjssj , solucione todos los casos posibles

--- practica 3/ejercicio 7/ejercicio7.pas
+++ practica 3/ejercicio 7/ejercicio7.pas
@@ -102,7 +102,7 @@ end;
 
 // PROCEDIMIENTOS PARA LA ELIMINACION Y COMPACTACION DE LOS ARCHIVOS.
 
-procedure eliminacionLogica(var a : archivo);
+procedure eliminacionLogica(var a : archivo; var cant_eliminados: integer);
 var
   cod : integer;
   reg : ave;  
@@ -126,6 +126,8 @@ begin
           reg.nombre:= 'eliminado';
           seek(a,filepos(a)-1);
           write(a,reg); 
+          // Sumo uno a los eliminados.
+          cant_eliminados:= cant_eliminados +1; 
           writeln('marcado como borrado. ');
         end
       else
@@ -139,72 +141,113 @@ end;
 
 
 
-procedure compactar(var archivo : archivo);
+procedure compactar(var archivo : archivo; cant_eliminados: integer);
 var
-  reg: ave; 
+  reg,aux: ave; 
   pos : integer; 
+  dl: integer;
+  borrados : integer;
 begin
-  // Debo recorrer el archivo desde principio a fin, y en cada posicion de eliminacion logica, tomo
-  // el ultimo archivo y lo intercambio por el ultimo - y trunco- 
+  {Debo recorrer el archivo hasta hacer la baja fisica de los eliminados, y en cada posicion de eliminacion logica, tomo
+   el ultimo elemento y lo intercambio por el ultimo. Ese ultimo puede no ser valido, por lo que debo buscar uno que si lo sea. 
   
-  // Se presenta una situacion donde pueden haber mas de un elemento al final del archivo
-  // eliminados de manera logica , asi q para trabajar de esta manera, me desago de ellos
-  // antes de reemplazar un eliminado por uno del final ej: 
-  // valido valido eliminado valido eliminado eliminado
-  // al querer reemplazar el primer eliminado por el ultimo estaria cometiendo un error, por ende 
-  // debo eliminar y truncar hasta tener uno valido en el final para poder reemplazar.
+  Se presenta una situacion donde puede haber mas de un elemento al final del archivo eliminados de manera logica, 
+  asi q para trabajar de esta manera, me desago de ellos antes de reemplazar un eliminado por uno del final 
+  * ej: [valido, valido, eliminado, valido, eliminado,eliminado]
+  
+  al querer reemplazar el primer eliminado por el ultimo estaria cometiendo un error, por ende debo buscar 
+  hasta tener uno valido en el final para poder reemplazar.
+  pero pueden ocurrir dos casos: 
+  *  Donde a partir de un elimando todos despues de ese sean eliminados [valido, eliminado, eliminado , eliminado]
+  * ,o el ya mencionado, entonces debemos manejar las posiciones del reg (pos) a reemplazar y el reg (pos) que encontre como valido.
+  
+  Por ultimo, puede ocurrir el caso que todos los archivos esten eliminados, en ese caso trunco al principio, lo manejo con contadores.
+  * }
   
   
   reset(archivo);
- 
-  //me posiciono al inicio del archivo 
-  seek(archivo,0);
-  leer(archivo,reg);
-  while (reg.cod <> valorAlto)do
+  // Para no recorrer todo el archivo, llevo un contador de borrados y corto cuando llegue a esa cantidad de borrados.
+  borrados:= 0;
+  // Uso dl como el ultimo registro valido que tengo en el archivo.
+  dl:= fileSize(archivo)-1;
+  if (cant_eliminados = (dl + 1))then
     begin
-      if (reg.nombre = 'eliminado')then
-        begin
-          // me guardo la posicion a reemplazar. es la anterior ya que al leerlo avanzo.
-          pos:= filepos(archivo)-1;
-          // Leo el ultimo registro para ver si es un eliminado o esta disponible.
-          seek(archivo,filesize(archivo)-1);
-          leer(archivo,reg);
-          // Si es un eliminado debo verificar que como ultimo me quede uno valido.
-          while(reg.nombre = 'eliminado' )do
-            begin
-              //al leerlo avanza al siguiente, entonces me posiono de nuevo en el que lei.
-              seek(archivo,filepos(archivo)-1);
-              // trunco el archivo en este punto.
-              truncate(archivo);
-              // como ya lo trunque , me posiciono de nuevo en el ultimo elemento.
-              seek(archivo,filesize(archivo)-1);
-              leer(archivo,reg);
-            end;
-          // me vuelvo a posicionar en este ya valido, y lo trunco asi pongo este en mi 'pos'.
-          seek(archivo,filesize(archivo)-1);
-          truncate(archivo);
-          // reemplazo el archivo eliminado.
-          seek(archivo,pos);
-          write(archivo,reg);
-        end; 
-      //leo el siguiente elemento.
-      leer(archivo,reg);
+      writeln('todos los archivos estan eliminados, no queda ningun elemento valido');
+      // Como esta posicionado en el principio del archivo trunco ahi 
+      truncate(archivo);
+    end
+  else
+    begin
+	  leer(archivo,reg);
+	  while ((reg.cod <> valorAlto) and (borrados <> cant_eliminados) )do
+		begin
+		  // Si encuentro un registro borrado entonces : 
+		  if (reg.nombre = 'eliminado')then
+			begin
+			  // me guardo la posicion a reemplazar. Es la anterior ya que al leerlo avanzo.
+			  pos:= filepos(archivo)-1;
+			  // Leo el ultimo registro para ver si es un eliminado o esta disponible.
+			  seek(archivo,dl);
+			  // Lo leo con auxiliar asi lo reemplazo, y no me quedan repetidos
+			  leer(archivo,aux);
+			  // Si es un eliminado debo verificar que como ultimo me quede uno valido en dl.
+			  while(aux.nombre = 'eliminado' )do
+				begin
+				  //Como al leerlo avanza al siguiente, entonces me posiono en el anterior al que lei.
+				  dl:= dl -1;
+				  seek(archivo,dl);
+				  leer(archivo,aux);
+				end;
+			  // Puede entrar o no al while dependiendo si el ultimo esta o no eliminado.
+			  // Entre o no voy a estar parado uno despues al registro valido seria dl +1.
+			  // Aca debo corroborar si a partir del que quiero reemplazar estan todos borrados o puedo hacer el intercambio: 
+			  if (pos>=dl)then
+				begin
+				  {Si la posicion a reemplazar es mas grande que mi dl(registro no borrado) entonces debo truncar todo a partir de pos
+				   ya que estan todos eliminados}
+				   // Entonces para que salga del while y trunque me posiciono el el fin del archivo para que tome valor alto. 
+				  //seek(archivo,filesize(archivo));
+				  borrados:= cant_eliminados;
+				end
+			  else
+				begin
+				  // Sino , puedo hacer el intercambio
+				  seek(archivo,dl);
+				  // Escribo en dl el borrado logicamente que estaba en pos
+				  write(archivo,reg);
+				  // Escribo en pos el registro valido e incremento en uno los borrados(reemplazados).
+				  seek(archivo,pos);
+				  write(archivo,aux);
+				  dl:= dl - 1; 
+				  borrados:= borrados + 1; 
+				end;
+			end; 
+		  //leo el siguiente elemento.
+		  leer(archivo,reg);
+		end; 
+	  // Cuando salgo debo truncar en dl +1 , ya que es el primer registro no valido.
+	  dl:= dl +1;
+	  seek(archivo,dl);
+	  truncate(archivo);
     end; 
   close(archivo);
 end; 
 
 
+
 var
   a : archivo; 
+  cant_eliminados: integer;
 BEGIN
   assign(a,'aves');
-  crearArchivo(a);
+  //crearArchivo(a);
   writeln('---------- archivo original -------------------------------------');
   imprimir(a);
-  eliminacionLogica(a);
+  eliminacionLogica(a,cant_eliminados);
   writeln('-----------archivo despues de la eliminacion logica -------------- ');
   imprimir(a);
-  compactar(a);
+  writeln('Cantidad de archivos eliminados : ', cant_eliminados);
+  compactar(a,cant_eliminados);
   writeln('-----------archivo final, compactado -----------------------------');
   imprimir(a);
 END.
